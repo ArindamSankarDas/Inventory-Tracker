95e984df3d70e742b4ec35290b7a5333e7b74519
Arindam Sankar Das <arindamsankar@outlook.com>
2024-05-09 16:50:27 +0530

    adjusted the productItem UI due to the css flex bug in the frontend

--- client/src/components/ProductItem/ProductItem.jsx
+++ client/src/components/ProductItem/ProductItem.jsx
@@ -40,23 +40,35 @@ const ProductItem = ({ productDetails }) => {
     }
 
     dispatch(updateProduct(productDetails.id, name, count, price));
-    setItemState({
-      name: productDetails.name,
-      count: productDetails.count,
-      price: productDetails.price,
-    });
     setToEdit(false);
   };
 
   return (
     <tr key={productDetails.id} className='w-full h-14 text-lg bg-secondary'>
-      <td className='relative top-3 flex justify-center gap-3 items-center'>
-        <div
-          className='bg-black px-2 py-2 rounded-full cursor-pointer inventory_hover_effect'
-          onClick={() => handleUpdate(productDetails)}
+      <td
+        className={`relative ${
+          toEdit ? "top-[1.8px]" : "top-3"
+        } flex justify-start gap-3 items-center lg:justify-center`}
+      >
+        <section
+          className={`flex justify-center ml-2 items-center gap-3 ${
+            !toEdit ? "lg:absolute" : "lg:static"
+          }  lg:left-5 lg:ml-0`}
         >
-          <Pencil color='black' size={"13px"} />
-        </div>
+          <div
+            className='bg-black px-2 py-2 rounded-full cursor-pointer inventory_hover_effect'
+            onClick={() => handleUpdate(productDetails)}
+          >
+            <Pencil color='black' size={"13px"} />
+          </div>
+          <div
+            className='bg-black px-2 py-2 rounded-full cursor-pointer inventory_hover_effect '
+            onClick={() => dispatch(deleteProduct(productDetails.id))}
+          >
+            <Trash2 color='black' size={"13px"} />
+          </div>
+        </section>
+
         {!toEdit ? (
           name
         ) : (
@@ -98,7 +110,7 @@ const ProductItem = ({ productDetails }) => {
           </div>
         )}
       </td>
-      <td className='flex justify-center items-center gap-4 lg:static'>
+      <td>
         {!toEdit ? (
           <>&#8377; {price}</>
         ) : (
@@ -118,12 +130,6 @@ const ProductItem = ({ productDetails }) => {
             />
           </div>
         )}
-        <div
-          className='bg-black px-2 py-2 rounded-full cursor-pointer inventory_hover_effect '
-          onClick={() => dispatch(deleteProduct(productDetails.id))}
-        >
-          <Trash2 color='black' size={"13px"} />
-        </div>
       </td>
     </tr>
   );
