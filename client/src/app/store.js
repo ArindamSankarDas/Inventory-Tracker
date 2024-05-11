bf64d389c1b98d55d4d19d70db130d4f9375ecad
Arindam Sankar Das <arindamsankar@outlook.com>
2024-05-11 11:06:41 +0530

    decided to remove the rtk query

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,15 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../features/products/productSlice";
 import transactionReducer from "../features/transactions/transactionSlice";
-import { apiSlice } from "./api/apiSlice";
 
 export const store = configureStore({
   reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
     products: productReducer,
     transactions: transactionReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
