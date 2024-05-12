2634cae2334dd1c1d77aa9e86fef978b42dedcc9
Arindam Sankar Das <arindamsankar@outlook.com>
2024-05-12 09:16:12 +0530

    added a conditional to render text when the data collection is empty

--- client/src/components/ItemsTable/ItemsTable.jsx
+++ client/src/components/ItemsTable/ItemsTable.jsx
@@ -128,18 +128,26 @@ const ItemsTable = ({ newProduct, setNewItem, data }) => {
       ) : null}
 
       <table className='w-full mt-5 text-center rounded-lg table_container overflow-hidden'>
-        <thead>
+        <thead className='relative'>
           <tr className='h-12 text-lg text-gray-600 bg-gray-300'>
             <th>Product</th>
             <th>Items</th>
             <th>Price</th>
           </tr>
         </thead>
-        <tbody>
-          {data.map((product) => (
-            <ProductItem key={product.id} productDetails={product} />
-          ))}
-        </tbody>
+        {!data?.length ? (
+          <tbody>
+            <tr className='absolute w-[90%] mt-3 left-1/2 -translate-x-1/2'>
+              No Items available
+            </tr>
+          </tbody>
+        ) : (
+          <tbody>
+            {data.map((product) => (
+              <ProductItem key={product.id} productDetails={product} />
+            ))}
+          </tbody>
+        )}
       </table>
     </div>
   );
