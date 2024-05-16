1ab6a75fb31a47ee553a2f027acac188c47cf08a
Arindam Sankar Das <arindamsankar@outlook.com>
2024-05-16 19:14:18 +0530

    removed async keyword

--- server/src/utils/hashmac_pwd.js
+++ server/src/utils/hashmac_pwd.js
@@ -1,6 +1,6 @@
 const crypto = require("node:crypto");
 
-const hashedPassword = async (algo, salt, pwd) =>
+const hashedPassword = (algo, salt, pwd) =>
   crypto.createHmac(algo, salt).update(pwd).digest("hex");
 
 module.exports = { hashedPassword };
