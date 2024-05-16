2e37d44b4fadb522dc53cee6415021a82cae7fa6
Arindam Sankar Das <arindamsankar@outlook.com>
2024-05-16 07:36:09 +0530

    1. installed new dependencies: jsonwebtoken, express-rate-limit, cookie-parser 2. added new middlewares to parse http-cookies and form data along with user and auth routes

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 // monodb config
 const connectDB = require("./config/db");
@@ -13,12 +14,17 @@ const port = process.env.PORT || 3000;
 // initiate database connection
 connectDB();
 
-// parse json requests
+// parse json and form requests
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cookieParser());
 
 // request routes
 app.use("/api/inventory", require("./routes/productRoute"));
 app.use("/api/transactions", require("./routes/transactionRoute"));
+app.use("/api/user", require("./routes/userRoute"));
+app.use("/api/auth", require("./routes/authRoute"));
 
 // establishing database connection
 mongoose.connection.once("open", () => {
