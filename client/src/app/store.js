28600d7c8006ad10fe8f0a073d9b8dcecd0f2b9f
Arindam Sankar Das <arindamsankar@outlook.com>
2024-05-17 08:11:40 +0530

    added userReducer to the store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../features/products/productSlice";
 import transactionReducer from "../features/transactions/transactionSlice";
+import userReducer from "../features/users/userSlice";
 
 export const store = configureStore({
   reducer: {
+    user: userReducer,
     products: productReducer,
     transactions: transactionReducer,
   },
